cfcb47510a06c4dc939ba170ef85f7449941f151
Андрей Сергеев <asergeev@me.com>
2020-09-25 14:47:17 +0300

    Lesson 8 Task 5

--- db/lesson_08/task_05.sql
+++ db/lesson_08/task_05.sql
@@ -10,60 +10,59 @@ USE vk;
 
 -- Ищем активность за последний год
 
+-- Используем вложенный SELECT
+SELECT 
+  CONCAT(first_name, ' ', last_name) AS user, 
+	(SELECT COUNT(*) FROM friendships f WHERE DATEDIFF(NOW(), f.requested_at) < 365 AND f.user_id = u.id) + 
+	(SELECT COUNT(*) FROM messages msg WHERE DATEDIFF(NOW(), msg.created_at) < 365 AND msg.from_user_id = u.id) + 
+	(SELECT COUNT(*) FROM likes l WHERE DATEDIFF(NOW(), l.created_at) < 365 AND l.user_id = u.id) +
+	(SELECT COUNT(*) FROM media m WHERE DATEDIFF(NOW(), m.created_at) < 365 AND m.user_id = u.id) +
+	(SELECT COUNT(*) FROM posts p WHERE DATEDIFF(NOW(), p.created_at) < 365 AND p.user_id = u.id)
+	AS overall_activity 
+	  FROM users u
+	  ORDER BY overall_activity
+	  LIMIT 10;
+
+
+-- Используем JOIN
 -- Пользователи, запросившие дружбу (user_id, requested_at)
-SELECT * FROM friendships f ;
-SELECT f.user_id user_id FROM friendships f WHERE  DATEDIFF(NOW(), f.requested_at) < 365;
+SELECT u.id, COUNT(f.user_id) FROM users u LEFT JOIN friendships f ON DATEDIFF(NOW(), f.requested_at) < 365 AND f.user_id = u.id GROUP BY u.id;
 
 -- Сообщения от пользователя (from_user_id, created_at)
-SELECT * FROM messages msg ;
-SELECT msg.from_user_id user_id FROM messages msg WHERE DATEDIFF(NOW(), msg.created_at) < 365;
+SELECT u.id, COUNT(msg.from_user_id) FROM users u LEFT JOIN messages msg ON DATEDIFF(NOW(), msg.created_at) < 365 AND msg.from_user_id = u.id GROUP BY u.id;
 
 -- Проставленные лайки (user_id, created_at)
-SELECT * FROM likes l ;
-SELECT l.user_id user_id FROM likes l WHERE DATEDIFF(NOW(), l.created_at) < 365;
+SELECT u.id, COUNT(l.user_id) FROM users u LEFT JOIN likes l ON DATEDIFF(NOW(), l.created_at) < 365 AND l.user_id = u.id GROUP BY u.id;
 
 -- Добавленный медиа-контент (user_id, created_at)
-SELECT * FROM media m ;
-SELECT m.user_id user_id FROM media m WHERE DATEDIFF(NOW(), m.created_at) < 365;
+SELECT u.id, COUNT(m.user_id) FROM users u LEFT JOIN media m ON DATEDIFF(NOW(), m.created_at) < 365 AND m.user_id = u.id GROUP BY u.id;
 
 -- Создание поста (user_id, created_at)
-SELECT * FROM posts p ;
-SELECT p.user_id user_id FROM posts p WHERE DATEDIFF(NOW(), p.created_at) < 365;
+SELECT u.id, COUNT(p.user_id) FROM users u LEFT JOIN posts p ON DATEDIFF(NOW(), p.created_at) < 365 AND p.user_id = u.id GROUP BY u.id;
 
 -- Добавление в группу и обновление профиля не считаем
 
--- Смотрим общую активность пользователей по показателям за последний год
-(SELECT f.user_id user_id FROM friendships f WHERE  DATEDIFF(NOW(), f.requested_at) < 365)
-UNION ALL 
-(SELECT msg.from_user_id user_id FROM messages msg WHERE DATEDIFF(NOW(), msg.created_at) < 365)
-UNION ALL
-(SELECT l.user_id user_id FROM likes l WHERE DATEDIFF(NOW(), l.created_at) < 365)
-UNION ALL
-(SELECT m.user_id user_id FROM media m WHERE DATEDIFF(NOW(), m.created_at) < 365)
-UNION ALL 
-(SELECT p.user_id user_id FROM posts p WHERE DATEDIFF(NOW(), p.created_at) < 365)
-ORDER BY user_id ;
-
--- Выбираем 10 из самых неактивных за последний год
+-- 10 самых неактивных
 SELECT 
-	CONCAT(u.first_name,' ',u.last_name) 
-FROM 
-	(SELECT user_id, COUNT(*) count_active 
-	FROM
-		(
-		(SELECT f.user_id user_id FROM friendships f WHERE  DATEDIFF(NOW(), f.requested_at) < 365)
-		UNION ALL 
-		(SELECT msg.from_user_id user_id FROM messages msg WHERE DATEDIFF(NOW(), msg.created_at) < 365)
-		UNION ALL
-		(SELECT l.user_id user_id FROM likes l WHERE DATEDIFF(NOW(), l.created_at) < 365)
-		UNION ALL
-		(SELECT m.user_id user_id FROM media m WHERE DATEDIFF(NOW(), m.created_at) < 365)
-		UNION ALL 
-		(SELECT p.user_id user_id FROM posts p WHERE DATEDIFF(NOW(), p.created_at) < 365)
-		) AS active_users
-	GROUP BY user_id
-	ORDER BY count_active
-	LIMIT 10) AS min_users
+	CONCAT(u2.first_name, ' ', u2.last_name) , SUM(activity.count_l) AS overall_activity 
+FROM
+	(
+	(SELECT u.id, COUNT(f.user_id) AS count_l FROM users u LEFT JOIN friendships f ON DATEDIFF(NOW(), f.requested_at) < 365 AND f.user_id = u.id GROUP BY u.id)
+	UNION ALL
+	(SELECT u.id, COUNT(msg.from_user_id) AS count_l FROM users u LEFT JOIN messages msg ON DATEDIFF(NOW(), msg.created_at) < 365 AND msg.from_user_id = u.id GROUP BY u.id)
+	UNION ALL
+	(SELECT u.id, COUNT(l.user_id) AS count_l FROM users u LEFT JOIN likes l ON DATEDIFF(NOW(), l.created_at) < 365 AND l.user_id = u.id GROUP BY u.id)
+	UNION ALL
+	(SELECT u.id, COUNT(m.user_id) AS count_l FROM users u LEFT JOIN media m ON DATEDIFF(NOW(), m.created_at) < 365 AND m.user_id = u.id GROUP BY u.id)
+	UNION ALL
+	(SELECT u.id, COUNT(p.user_id) AS count_l FROM users u LEFT JOIN posts p ON DATEDIFF(NOW(), p.created_at) < 365 AND p.user_id = u.id GROUP BY u.id)
+	) AS activity
 JOIN 
-	users u 
-WHERE min_users.user_id = u.id ;
+	users u2
+ON 
+	activity.id = u2.id
+GROUP BY 
+	activity.id
+ORDER BY 
+	overall_activity 
+LIMIT 10;
